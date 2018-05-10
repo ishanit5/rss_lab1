eb8f46be2facd8ee36db20ef2c414a23790226b9
ishanit5 <ishani.thakur@gmail.com>
2018-05-09 22:23:01 -0400

    fake laser scan data publishes to two topics

--- rss_lab1/src/ros_exercises/scripts/open_space_publisher.py
+++ rss_lab1/src/ros_exercises/scripts/open_space_publisher.py
@@ -2,19 +2,21 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Header
+import OpenSpace.msg
 import random
 import math
 from std_msgs.msg import Float32
+dist = rospy.Publisher('open_space/distance', Float32, queue_size=10)
+ang = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+d = 0.0
+a = 0.0
 def callback(data):
-    dist = rospy.Publisher('open_space/distance', Float32, queue_size=10)
-    ang = rospy.Publisher('open_space/angle', Float32, queue_size=10)
     d, a = process(data.ranges)
     #print("a is: " + str(a))
     rospy.loginfo(d)
     rospy.loginfo(a)
     dist.publish(d)
     ang.publish(a)
-
 def process(ranges):
     largest = -1.0
     ang = 0.0
@@ -34,7 +36,6 @@ def listener():
     rospy.init_node('open_space_publisher', anonymous=True)
 
     rospy.Subscriber('fake_scan', LaserScan, callback)
-
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
